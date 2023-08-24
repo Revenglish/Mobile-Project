da6f9b075810af3ec6d063c23f459f182e9a5c24
Davi-senna <98260347+Davi-senna@users.noreply.github.com>
2023-08-24 14:41:17 -0300

    Structure of header finished

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,17 +3,35 @@ import { StyleSheet, Text, View, Image} from 'react-native';
 export default function Header() {
     return (
         <View style={styles.header}>
-            <Image source={require('../../assets/notification.png')} />
-        </View>
+            <Image style={styles.notificationImage} source={require('../../assets/notification.png')} />
+            <Text style={styles.title}>RevEnglish</Text>
+            <Image style={styles.settingsImage} source={require('../../assets/settings.png')} />
+        </View >
     );
 }
 
 const styles = StyleSheet.create({
     header: {
-        alignItems: 'space-between',
-        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         backgroundColor: '#D9D9D9',
+        paddingLeft: '7%',
+        paddingRight: '7%',
         width: '100%',
         height: '21%'
     },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+    },
+    settingsImage: {
+        width: 45,
+        height: 45,
+    },
+    notificationImage: {
+        width: 47,
+        height: 47,
+    }
 })
